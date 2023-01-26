cdf85fbb0474a5f760e56231f771f05a9a417344
Oumniaa <115029384+Oumniaa@users.noreply.github.com>
2023-01-26 17:38:27 +0100

    Create bluetooth.ino

---
+++ code/bras/bluetooth.ino
@@ -0,0 +1,67 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+Servo servo01;
+Servo servo02;
+Servo servo03;
+Servo servo04;
+Servo servo05;
+Servo servo06;
+
+SoftwareSerial Bluetooth(3, 4); // Arduino(RX, TX) - HC-05 Bluetooth (TX, RX)
+
+int servo1Pos, servo2Pos, servo3Pos, servo4Pos, servo5Pos, servo6Pos; // current position
+int servo1PPos, servo2PPos, servo3PPos, servo4PPos, servo5PPos, servo6PPos; // previous position
+int servo01SP[50], servo02SP[50], servo03SP[50], servo04SP[50], servo05SP[50], servo06SP[50]; // for storing positions/steps
+int speedDelay = 20;
+int index = 0;
+String dataIn = "";
+void setup() {
+  servo01.attach(5);
+  servo02.attach(6);
+  servo03.attach(7);
+  servo04.attach(8);
+  servo05.attach(9);
+  servo06.attach(10);
+  Bluetooth.begin(38400); // Default baud rate of the Bluetooth module
+  Bluetooth.setTimeout(1);
+  delay(20);
+  // Robot arm initial position
+  servo1PPos = 90;
+  servo01.write(servo1PPos);
+  servo2PPos = 150;
+  servo02.write(servo2PPos);
+  servo3PPos = 35;
+  servo03.write(servo3PPos);
+  servo4PPos = 140;
+  servo04.write(servo4PPos);
+  servo5PPos = 85;
+  servo05.write(servo5PPos);
+  servo6PPos = 80;
+  servo06.write(servo6PPos);
+}
+// If "Waist" slider has changed value - Move Servo 1 to position
+    if (dataIn.startsWith("s1")) {
+      String dataInS = dataIn.substring(2, dataIn.length()); // Extract only the number. E.g. from "s1120" to "120"
+      servo1Pos = dataInS.toInt();  // Convert the string into integer
+      // We use for loops so we can control the speed of the servo
+      // If previous position is bigger then current position
+      if (servo1PPos > servo1Pos) {
+        for ( int j = servo1PPos; j >= servo1Pos; j--) {   // Run servo down
+          servo01.write(j);
+          delay(20);    // defines the speed at which the servo rotates
+        }
+      }
+      // If previous position is smaller then current position
+      if (servo1PPos < servo1Pos) {
+        for ( int j = servo1PPos; j <= servo1Pos; j++) {   // Run servo up
+          servo01.write(j);
+          delay(20);
+        }
+      }
+      servo1PPos = servo1Pos;   // set current position as previous position
+    }
+/*
+  the bluetooth module is not yet fully functional so I stopped at this test part, we manage to send an instruction but the reception is not done.
+  the time to solve this problem we will continue to use the potentiometer.
+*/
